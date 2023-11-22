25a658f2b5e53a387f571a092ff8bb191430e55f
JKevin2713 <kevin201560@estudiantec.cr>
2023-11-22 13:24:00 -0600

    init project

---
+++ ProyectoBases/ProyectoBases/Forms/Inico.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using Model;
+//using Planta;
+
+namespace ProyectoBases
+{
+    public partial class Inico : Form
+    {
+        public Inico()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Plantas PlantasInicio = new Plantas(); // Crea una nueva instancia de la segunda ventana
+            PlantasInicio.ShowDialog(); // Muestra la segunda ventana
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Planta NewPlanta = new Planta();
+            NewPlanta.Nombre = "Santa Cruz";
+            NewPlanta.Conexion = true;
+
+            DB.PlantaDB planta = new DB.PlantaDB();
+            planta.InsertarPlanta(NewPlanta);
+            
+        }
+
+    }
+}
---
+++ ProyectoBases/ProyectoBases/Utils/Constantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Constantes
+    {
+        public const string ConnectionString = "server=localhost;user=root;database=PlantaParrita;port=3306;password=jk2713";
+
+    }
+}
